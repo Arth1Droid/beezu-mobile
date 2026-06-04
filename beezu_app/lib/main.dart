d81ae55c5062349a77b9ab1cc94e170e46a11f5f
barbara paranhos <barbparanhos@gmail.com>
2026-06-04 15:59:37 -0300

    feat: supabase inicializado e login do google 100% configurado

--- beezu_app/lib/main.dart
+++ beezu_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'auth_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +10,8 @@ void main() async {
     url: 'https://gxfmpvbmwmpeiabsjjaj.supabase.co',
     anonKey: 'sb_publishable_iJgboQFVoqbLU-RT3YNlVw_lK2suyEZ',
   );
-
+print(' [BEEZU BACKEND] CONEXÃO COM O SUPABASE ESTABELECIDA COM SUCESSO!');
+AuthService().signInWithGoogle();
   runApp(const MyApp());
 }
 
